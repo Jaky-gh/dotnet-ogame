3b65a7c42a2d1fe929f46a9782a5c45f217f8697
Romain Mounier <romain.mounier@epita.fr>
2019-05-09 14:56:59 +0200

    add sample project

---
+++ Ogame/Ogame/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ogame.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
