d5fe8a0307f398d6c397176a1b94d0ca65b4c6b1
Anthony Truong <anthonytruong@hotmail.fr>
2019-05-13 18:30:34 +0200

    [UPDATE] Hidding edit button for regular users in details view

---
+++ Ogame/Ogame/Models/SpaceshipView/SpaceshipDetailsViewInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ogame.Models.SpaceshipView
+{
+    public class SpaceshipDetailsViewInterface
+    {
+        public Spaceship _spaceship { get; set; }
+        public User _user { get; set; }
+
+        public SpaceshipDetailsViewInterface(Spaceship spaceship, User user)
+        {
+            _spaceship = spaceship;
+            _user = user;
+        }
+    }
+}
