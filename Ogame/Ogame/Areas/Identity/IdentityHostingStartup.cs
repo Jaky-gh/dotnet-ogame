1769c7f7cc6fe52158ddef3adac01b70b575ea2d
Anthony Truong <anthonytruong@hotmail.fr>
2019-05-14 16:04:58 +0200

    [ADD] Scaffolding for login, logout, register

---
+++ Ogame/Ogame/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ogame.Data;
+using Ogame.Models;
+
+[assembly: HostingStartup(typeof(Ogame.Areas.Identity.IdentityHostingStartup))]
+namespace Ogame.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
